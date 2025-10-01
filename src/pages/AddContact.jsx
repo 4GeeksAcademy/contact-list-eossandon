3d0a2486825807a36a8591df662e1f147bd3a37b
Carlos Schile <esteban.ossandon20@gmail.com>
2025-10-01 17:27:47 -0300

    se agrega api POST y DEL

--- src/pages/AddContact.jsx
+++ src/pages/AddContact.jsx
@@ -1,39 +1,73 @@
 import useGlobalReducer from "../hooks/useGlobalReducer";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
+
+
+const API_URL = "https://playground.4geeks.com/contact";
 
 
 export const AddContact = () => {
+    const [inputName, setInputName] = useState("");
+    const [inputPhone, setInputPhone] = useState("");
+    const [inputEmail, setInputEmail] = useState("");
+    const [inputAddress, setInputAddress] = useState("");
+
+    const postContact = async () => {
+        try {
+            console.log(inputName + inputPhone + inputEmail + inputAddress)
+            const response = await fetch(`${API_URL}/agendas/esteban/contacts`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: inputName,
+                    phone: inputPhone,
+                    email: inputEmail,
+                    address: inputAddress,
+                }),
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
     return (
         <div className="container mt-5">
-            <form className="form">
-                <div class="mb-3">
-                    <label for="basic-url" class="form-label">Full Name:</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4" />
+            <form className="form" onSubmit={(e) => { e.preventDefault(); postContact(); }}>
+                <div className="mb-3">
+                    <label htmlFor="basic-name" className="form-label">Full Name:</label>
+                    <div className="input-group">
+                        <input type="text" className="form-control" id="basic-name" aria-describedby="basic-addon3 basic-addon4" onChange={(e) => setInputName(e.target.value)} required />
                     </div>
                 </div>
 
-                <div class="mb-3">
-                    <label for="basic-url" class="form-label">Address:</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4" />
+                <div className="mb-3">
+                    <label htmlFor="basic-address" className="form-label">Address:</label>
+                    <div className="input-group">
+                        <input type="text" className="form-control" id="basic-address" aria-describedby="basic-addon3 basic-addon4" onChange={(e) => setInputAddress(e.target.value)} required />
                     </div>
                 </div>
 
-                <div class="mb-3">
-                    <label for="basic-url" class="form-label">Phone:</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4" />
+                <div className="mb-3">
+                    <label htmlFor="basic-phone" className="form-label">Phone:</label>
+                    <div className="input-group">
+                        <input type="text" className="form-control" id="basic-phone" aria-describedby="basic-addon3 basic-addon4" onChange={(e) => setInputPhone(e.target.value)} required />
                     </div>
                 </div>
 
-                <div class="mb-3">
-                    <label for="basic-url" class="form-label">Email:</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4" />
+                <div className="mb-3">
+                    <label htmlFor="basic-email" className="form-label">Email:</label>
+                    <div className="input-group">
+                        <input type="text" className="form-control" id="basic-email" aria-describedby="basic-addon3 basic-addon4" onChange={(e) => setInputEmail(e.target.value)} required />
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <div>
+                    <button type="submit" className="btn btn-primary w-100" >Submit</button>
+                </div>
             </form>
 
         </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -24,6 +24,17 @@ export const Contact = () => {
         }
     };
 
+    const delContact = async (id) => {
+        try {
+            const response = await fetch(`${API_URL}/agendas/esteban/contacts/${id}`, {
+                method: "DELETE",
+            });
+            getAllContact();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     useEffect(() => {
         getAllContact();
     }, []);
@@ -37,7 +48,7 @@ export const Contact = () => {
                 </Link>
             </div>
             {contacts.map((contacts, index) => (
-                <div key={index} className="row border  mb-0">
+                <div key={index} className="row border mb-0">
                     <div className="col-lg-2 col-md-4 col-sm-4 d-flex text-align-center justify-content-center">
                         <img className="img-contact" src={Image} alt="" />
                     </div>
@@ -50,11 +61,29 @@ export const Contact = () => {
                     </div>
 
                     <div className="col-lg-3 col-md-2 col-sm-3 d-flex text-align-end justify-content-end ">
-                        <i className="bi bi-pencil-fill p-4"></i>
-                        <i className="bi bi-trash-fill p-4"></i>
+                        <i className="bi bi-pencil-fill p-4 ex"></i>
+                        <i className="bi bi-trash-fill p-4 ex" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+                    </div>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Delete contacts</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ¿Desea eliminar este contacto?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                    <button type="button" class="btn btn-primary" onClick={() => delContact(contacts.id)} data-bs-dismiss="modal" >Delete</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             ))}
+
         </div>
     );
 
